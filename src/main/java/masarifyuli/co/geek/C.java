f702832d741af8f1616c9e9a80b167387c0dd9a6
masarifyuli <arifyuli15@gmail.com>
2023-03-24 10:45:15 +0700

    post method

--- src/main/java/masarifyuli/co/geek/C.java
+++ src/main/java/masarifyuli/co/geek/C.java
@@ -2,7 +2,7 @@ package masarifyuli.co.geek;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping(value = "/app")
 public class C {
 
-    @GetMapping(value = "fibonacci")
+    @PostMapping(value = "fibonacci")
     public ResponseEntity<Integer> getFibonacci(@RequestParam int index) {
         return ResponseEntity.ok(new U().getFibonacci(index));
     }
 
-    @GetMapping(value = "nonPalindrom")
+    @PostMapping(value = "nonPalindrom")
     public ResponseEntity<String> palindrom(@RequestParam String key) {
         return ResponseEntity.ok(new U().nonPalindrom(key));
     }
